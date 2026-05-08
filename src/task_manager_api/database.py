01dc54e90e3e75fd5355c0ce1f5669d99454395c
Ayikoru Alima <alima.ayikoru1@gmail.com>
2026-05-08 10:15:37 +0300

    chore: setup database configurations and session dependency

---
+++ src/task_manager_api/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import DeclarativeBase
+from fastapi import Request
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_session(request: Request):
+    async with request.app.state.session_factory() as session:
+        yield session
---
+++ src/task_manager_api/main.py
@@ -0,0 +1,34 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy import select, text
+from src.task_manager_api.settings import settings
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    print("Stating up the application...")
+    engine = create_async_engine(settings.database_url, echo=True)
+
+    try:
+        async with engine.connect() as conn:
+            # await conn.execute(select(1))
+            await conn.execute(text("SELECT 1"))
+            print("Database connection successful")
+    except Exception as e:
+        print(f"Database connection failed: {e}")
+        raise ConnectionError("Failed to connect to the database") from e
+    
+    app.state.session_factory = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+    yield
+
+    print("Shutting down the application...")
+    await engine.dispose()
+
+# Create the FastAPI application with the defined lifespan
+app = FastAPI(lifespan=lifespan)
+
+    
