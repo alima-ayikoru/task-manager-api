c2679f8420643dff9a1fdf4964d825f90fb44320
Ayikoru Alima <alima.ayikoru1@gmail.com>
2026-05-12 07:33:48 +0300

    feat: add Task model

---
+++ src/task_manager_api/models/task.py
@@ -0,0 +1,22 @@
+from src.task_manager_api.database import Base
+from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy.sql import func
+from src.task_manager_api.enums import TaskStatus
+
+
+class Task(Base):
+    __tablename__="tasks"
+
+    task_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
+    project_id: Mapped[int | None] = mapped_column(ForeignKey("projects.project_id"))
+    title: Mapped[str] = mapped_column()
+    description: Mapped[str | None] = mapped_column()
+    start_date: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    due_date: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    status: Mapped[TaskStatus] = mapped_column(default=TaskStatus.PENDING)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default= func.now())
+    updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), onupdate=func.now())
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
