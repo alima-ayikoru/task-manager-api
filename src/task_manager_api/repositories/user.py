833095a470e674f0f7913a7b206822709d2ff54c
Ayikoru Alima <alima.ayikoru1@gmail.com>
2026-06-03 22:04:14 +0300

    fix: correct the import path from which User is imported

--- src/task_manager_api/repositories/user.py
+++ src/task_manager_api/repositories/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.task_manager_api.models import User
+from src.task_manager_api.models.user import User
 
 
 class UserRepository:
